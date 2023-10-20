92fbbae0a32e9368bcaad02d9a0761d924f71fc1
Alonsomaureiravidal <72326696+Alonsomaureiravidal@users.noreply.github.com>
2023-10-19 23:29:27 -0300

    Update mostrar.js

--- src/components/mostrar.js
+++ src/components/mostrar.js
@@ -4,8 +4,8 @@ import '/estilos/mostrar.css';
 
 function mostrar() {
     const data = [
-        {importancia: 2, nombre: 'Av Grecia 123', descripcion: 'Reparación de luminarias', fecha: '2023-10-20'},
-        {importancia: 3, nombre: 'Moneda 748', descripcion: 'Semaforo apagado', fecha: '2023-10-21'},
+        {importancia: 2, nombre: 'Av Grecia 123', descripcion: 'Reparación de luminarias', fecha: '2023-10-18'},
+        {importancia: 3, nombre: 'Moneda 748', descripcion: 'Semaforo apagado', fecha: '2023-10-13'},
         {importancia: 2, nombre: 'Esmeralda 23', descripcion: 'Pavimento en mal estado', fecha: '2023-10-19'},
         {importancia: 1, nombre: 'Caupolicán 239', descripcion: 'Retiro de escombros', fecha: '2023-10-05'},
         // Agrega más datos aquí...
