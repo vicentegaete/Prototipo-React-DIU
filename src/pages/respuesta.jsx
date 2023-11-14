f43419fac022e37472ae9e64dcf88bbff3e08f13
Alonsomaureiravidal <72326696+Alonsomaureiravidal@users.noreply.github.com>
2023-11-13 21:47:29 -0300

    Update respuesta.jsx

--- src/pages/respuesta.jsx
+++ src/pages/respuesta.jsx
@@ -3,8 +3,6 @@ import { useLocation } from 'react-router-dom';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-
-
 export const RespuestaPage = () => {
     const location = useLocation();
     const opcion = new URLSearchParams(location.search).get('opcion');
@@ -17,22 +15,19 @@ export const RespuestaPage = () => {
         opcion5: 'Poda de arboles',
     };
 
-    // Obtén la fecha actual
     const currentDate = new Date();
     const currentYear = currentDate.getFullYear();
-    const currentMonth = String(currentDate.getMonth() + 1).padStart(2, '0'); // Los meses van de 0 a 11, por lo que añadimos 1
+    const currentMonth = String(currentDate.getMonth() + 1).padStart(2, '0');
     const currentDay = String(currentDate.getDate()).padStart(2, '0');
 
-    // Formatea la fecha al formato 'YYYY-MM-DD'
     const formattedDate = `${currentYear}-${currentMonth}-${currentDay}`;
 
     const [formularioData, setFormularioData] = useState({
         descripcion: descripcionPorOpcion[opcion] || opcion,
         ubicacion: '',
-        fecha: formattedDate, // Establece la fecha actual como valor predeterminado
+        fecha: formattedDate,
     });
 
-    // Establece las fechas mínima y máxima para el año actual
     const minDate = `${currentYear}-01-01`;
     const maxDate = `${currentYear}-12-31`;
 
@@ -58,43 +53,44 @@ export const RespuestaPage = () => {
     return (
         <div className="page">
             <h1 className="page__title">MuniDenuncia</h1>
-            <h2>Respuesta a la opción: {descripcionPorOpcion[opcion]}</h2> {/* Muestra el nombre de la opción seleccionada */}
+            <h2>Respuesta a la opción: {descripcionPorOpcion[opcion]}</h2>
             {!reporteEnviado && (
                 <form onSubmit={handleSubmit}>
                     {opcion === 'opcion6' ? (
-    <div>
-        <label htmlFor="descripcion">Descripción:</label>
-        <input
-            type="text"
-            id="descripcion"
-            name="descripcion"
-            onChange={handleInputChange}
-        />
-        <label htmlFor="ubicacion">Ubicación:</label>
-        <input
-            type="text"
-            id="ubicacion"
-            name="ubicacion"
-            value={formularioData.ubicacion}
-            onChange={handleInputChange}
-        />
-    </div>
-) : (
-<div>
-    <label htmlFor="ubicacion">Ubicación:</label>
-    <input
-        type="text"
-        id="ubicacion"
-        name="ubicacion"
-        value={formularioData.ubicacion}
-        onChange={handleInputChange}
-    />
-    <br />
-    <label htmlFor="fecha">Fecha:</label>
-    <p id="fecha">{formularioData.fecha}</p> {/* Muestra la fecha como texto en lugar de un campo de entrada */}
-</div>
-)}
-
+                        <div>
+                            <label htmlFor="descripcion">Descripción:</label>
+                            <input
+                                type="text"
+                                id="descripcion"
+                                name="descripcion"
+                                onChange={handleInputChange}
+                            />
+                            <label htmlFor="ubicacion">Ubicación:</label>
+                            <input
+                                type="text"
+                                id="ubicacion"
+                                name="ubicacion"
+                                value={formularioData.ubicacion}
+                                onChange={handleInputChange}
+                            />
+                            <label htmlFor="fecha">Fecha:</label>
+                            <p id="fecha">{formularioData.fecha}</p>
+                        </div>
+                    ) : (
+                    <div>
+                        <label htmlFor="ubicacion">Ubicación:</label>
+                        <input
+                            type="text"
+                            id="ubicacion"
+                            name="ubicacion"
+                            value={formularioData.ubicacion}
+                            onChange={handleInputChange}
+                        />
+                        <br />
+                        <label htmlFor="fecha">Fecha:</label>
+                        <p id="fecha">{formularioData.fecha}</p>
+                    </div>
+                    )}
                     <Button type="submit">Enviar</Button>
                 </form>
             )}
@@ -111,4 +107,5 @@ export const RespuestaPage = () => {
 };
 
 export default RespuestaPage;
+
 
